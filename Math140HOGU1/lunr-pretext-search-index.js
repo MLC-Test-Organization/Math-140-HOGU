057d99807e8149646e91a3f465ba30c2ab1c20dd
Jennifer Whitfield <jwhitfld@tamu.edu>
2026-08-22 13:45:20 +0000

    Latest build deployed.

---
+++ Math140HOGU1/lunr-pretext-search-index.js
@@ -0,0 +1,158 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "0001-3",
+  "level": "1",
+  "url": "0001-3.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Session #1 (Sections 1.1 &amp; 1.2)",
+  "body": " Session #1 (Sections 1.1 & 1.2)  Directions: Answer each of the following as best you can. Feel free to discuss your thoughts about the questions with some peers around you. The Math 140 textbook is free, and can be viewed at Texas A&M OAKTrust Digital Repository .    Getting Warmed Up and in the Right Mindset    Use the matrices, given below, to complete the following tasks.      Matrix has dimensions because it has ____ rows and ____ columns.    Matrix has dimensions because it has rows and columns.      The dimensions of matrix are ____ ____ because it has ____ rows and ____ columns.    The dimensions of matrix are because it has rows and columns.      Because the dimensions of matrix are ____ ____, the dimensions of are ____ ____.    Because the dimensions of matrix are , the dimensions of are .      Answer True or False for each of the following.    is a defined matrix.     is a defined matrix.    The dimensions of are .                 False, True, False, False, True      State the value of each entry:    ____     ____        ,        Let Us Get Our Hands On    Given the matrices below, determine if the following computations are possible. If the computations are possible, find the resulting matrix by hand (i.e., do the math without a calculator), then check your answer using a calculator. If the computation is not possible, then state why.                      Since is a matrix, and is a matrix, and have different dimensions so matrix subtraction is not defined and the given operation is not possible.                               Find a matrix such that the following matrix equation is true.            Find the values of and that make the matrix equation true.      ,       Given matrices and ...     ...find .           ...find .           ...is ?    Yes, from parts (a) and (b). (In general, for all matrices and .)       Laura is planning to buy sugar, flour, milk, and eggs for her bakery business. The prices of these items in three neighborhood supermarkets are shown in the table below.  Supermarket Item Prices    Supermarkets  Sugar ( -lb bag)  Flour ( -lb bag)  Milk ( -gal carton)  Eggs ( -dozen count carton)    Supermarket 1        Supermarket 2        Supermarket 3             Write a matrix to represent the prices of the four items in the three supermarkets.           For the first week, Laura plans to buy three -lb bags of sugar, six -lb bags of flour, four -gal cartons of milk, and four -dozen count cartons of eggs. Write a matrix to represent the quantities of each item that Laura plans to purchase for the first week.           For the second week, Laura plans to buy two -lb bags of sugar, three -lb bags of flour, five -gal cartons of milk, and two -dozen count cartons of eggs. Write a matrix to represent the quantities of each item that Laura plans to purchase for the second week.           Find and interpret the meaning of the matrix.     represents the quantity of each item that Laura plans to buy for the first and second weeks combined.      If , what does matrix mean?     represents the matrix of total cost Laura would spend on the items purchased for the first and second weeks in each supermarket.      Use your calculator to find matrix and then determine which supermarket Laura should buy the items from to minimize the cost. (Assuming she only shops at one supermarket)     , so Laura should purchase items from Supermarket 3 to minimize her total cost.       Consider the matrices and . Find the values of and so that .     ,       A company tracks sales of three products ( , , and ) across four regions ( , , , and ). Matrix below represents the amount of each product sold in each region.      If the prices of Products , , and are , , and respectively, create a matrix that represents the price of each product.           Write an equation using matrices and that represents the revenue matrix .           Find the revenue matrix and interpret the meaning of .     , is the revenue from the sales of products , , and in region .       Matrix represents the prices for one faculty ticket to three different Aggie football games. Matrix represents the number of faculty members who attended each of the three games. Compute the product and interpret the result.     , so, in total, faculty members spend on tickets to attend Aggie football games , , and .      Let , , and be matrices such that has dimensions and has dimensions .     Find the dimensions of the matrix so that the product matrix exists.           What are the dimensions of the matrix ?            Now Grades Go Up    If is a matrix and is a matrix, what are the dimensions of ?                        Not defined      (A)      Let and . If , find the value of .                               (C)      If is a matrix, is a matrix, and is a matrix, which one of the following matrix operations is defined?                               (B)      An auto parts store has reduced the price of three items (wiper blades, brake fluid, and floor mats) in their stores by . The original price at Store for wiper blades was , a can of brake fluid was , and floor mats were . Which answer below is the correct matrix that represents this information?                        None of these      (A)      Given , , and , find the value of .                         is not a defined matrix      (B)     "
+},
+{
+  "id": "0001-3-3-2",
+  "level": "2",
+  "url": "0001-3.html#0001-3-3-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Use the matrices, given below, to complete the following tasks.      Matrix has dimensions because it has ____ rows and ____ columns.    Matrix has dimensions because it has rows and columns.      The dimensions of matrix are ____ ____ because it has ____ rows and ____ columns.    The dimensions of matrix are because it has rows and columns.      Because the dimensions of matrix are ____ ____, the dimensions of are ____ ____.    Because the dimensions of matrix are , the dimensions of are .      Answer True or False for each of the following.    is a defined matrix.     is a defined matrix.    The dimensions of are .                 False, True, False, False, True      State the value of each entry:    ____     ____        ,     "
+},
+{
+  "id": "0001-3-4-2",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Given the matrices below, determine if the following computations are possible. If the computations are possible, find the resulting matrix by hand (i.e., do the math without a calculator), then check your answer using a calculator. If the computation is not possible, then state why.                      Since is a matrix, and is a matrix, and have different dimensions so matrix subtraction is not defined and the given operation is not possible.                            "
+},
+{
+  "id": "0001-3-4-3",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Find a matrix such that the following matrix equation is true.         "
+},
+{
+  "id": "0001-3-4-4",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Find the values of and that make the matrix equation true.      ,    "
+},
+{
+  "id": "0001-3-4-5",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Given matrices and ...     ...find .           ...find .           ...is ?    Yes, from parts (a) and (b). (In general, for all matrices and .)    "
+},
+{
+  "id": "0001-3-4-6",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-6",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Laura is planning to buy sugar, flour, milk, and eggs for her bakery business. The prices of these items in three neighborhood supermarkets are shown in the table below.  Supermarket Item Prices    Supermarkets  Sugar ( -lb bag)  Flour ( -lb bag)  Milk ( -gal carton)  Eggs ( -dozen count carton)    Supermarket 1        Supermarket 2        Supermarket 3             Write a matrix to represent the prices of the four items in the three supermarkets.           For the first week, Laura plans to buy three -lb bags of sugar, six -lb bags of flour, four -gal cartons of milk, and four -dozen count cartons of eggs. Write a matrix to represent the quantities of each item that Laura plans to purchase for the first week.           For the second week, Laura plans to buy two -lb bags of sugar, three -lb bags of flour, five -gal cartons of milk, and two -dozen count cartons of eggs. Write a matrix to represent the quantities of each item that Laura plans to purchase for the second week.           Find and interpret the meaning of the matrix.     represents the quantity of each item that Laura plans to buy for the first and second weeks combined.      If , what does matrix mean?     represents the matrix of total cost Laura would spend on the items purchased for the first and second weeks in each supermarket.      Use your calculator to find matrix and then determine which supermarket Laura should buy the items from to minimize the cost. (Assuming she only shops at one supermarket)     , so Laura should purchase items from Supermarket 3 to minimize her total cost.    "
+},
+{
+  "id": "0001-3-4-7",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-7",
+  "type": "Worksheet Exercise",
+  "number": "6",
+  "title": "",
+  "body": "  Consider the matrices and . Find the values of and so that .     ,    "
+},
+{
+  "id": "0001-3-4-8",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-8",
+  "type": "Worksheet Exercise",
+  "number": "7",
+  "title": "",
+  "body": "  A company tracks sales of three products ( , , and ) across four regions ( , , , and ). Matrix below represents the amount of each product sold in each region.      If the prices of Products , , and are , , and respectively, create a matrix that represents the price of each product.           Write an equation using matrices and that represents the revenue matrix .           Find the revenue matrix and interpret the meaning of .     , is the revenue from the sales of products , , and in region .    "
+},
+{
+  "id": "0001-3-4-9",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-9",
+  "type": "Worksheet Exercise",
+  "number": "8",
+  "title": "",
+  "body": "  Matrix represents the prices for one faculty ticket to three different Aggie football games. Matrix represents the number of faculty members who attended each of the three games. Compute the product and interpret the result.     , so, in total, faculty members spend on tickets to attend Aggie football games , , and .   "
+},
+{
+  "id": "0001-3-4-10",
+  "level": "2",
+  "url": "0001-3.html#0001-3-4-10",
+  "type": "Worksheet Exercise",
+  "number": "9",
+  "title": "",
+  "body": "  Let , , and be matrices such that has dimensions and has dimensions .     Find the dimensions of the matrix so that the product matrix exists.           What are the dimensions of the matrix ?         "
+},
+{
+  "id": "0001-3-5-2",
+  "level": "2",
+  "url": "0001-3.html#0001-3-5-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  If is a matrix and is a matrix, what are the dimensions of ?                        Not defined      (A)   "
+},
+{
+  "id": "0001-3-5-3",
+  "level": "2",
+  "url": "0001-3.html#0001-3-5-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Let and . If , find the value of .                               (C)   "
+},
+{
+  "id": "0001-3-5-4",
+  "level": "2",
+  "url": "0001-3.html#0001-3-5-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  If is a matrix, is a matrix, and is a matrix, which one of the following matrix operations is defined?                               (B)   "
+},
+{
+  "id": "0001-3-5-5",
+  "level": "2",
+  "url": "0001-3.html#0001-3-5-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  An auto parts store has reduced the price of three items (wiper blades, brake fluid, and floor mats) in their stores by . The original price at Store for wiper blades was , a can of brake fluid was , and floor mats were . Which answer below is the correct matrix that represents this information?                        None of these      (A)   "
+},
+{
+  "id": "0001-3-5-6",
+  "level": "2",
+  "url": "0001-3.html#0001-3-5-6",
+  "type": "Worksheet Exercise",
+  "number": "5",
+  "title": "",
+  "body": "  Given , , and , find the value of .                         is not a defined matrix      (B)   "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
